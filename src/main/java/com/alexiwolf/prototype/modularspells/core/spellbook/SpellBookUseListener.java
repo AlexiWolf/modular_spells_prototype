2dadc53d06a8c0e952bd7c2e302af567a8f8ca9d
AlexiWolf <alexiwolf@pm.me>
2021-05-30 09:20:11 -0500

    Handle gui clicks

--- src/main/java/com/alexiwolf/prototype/modularspells/core/spellbook/SpellBookUseListener.java
+++ src/main/java/com/alexiwolf/prototype/modularspells/core/spellbook/SpellBookUseListener.java
@@ -3,12 +3,15 @@ package com.alexiwolf.prototype.modularspells.core.spellbook;
 import com.alexiwolf.prototype.modularspells.ModularSpells;
 import com.alexiwolf.prototype.modularspells.core.providers.SpellProvider;
 import com.alexiwolf.prototype.modularspells.core.spells.Spell;
+import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -16,10 +19,14 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpellBookUseListener implements Listener {
 
     private final ModularSpells plugin;
     private final SpellProvider spellProvider;
+    private final List<Inventory> openSpellBooks = new ArrayList<>();
 
     public SpellBookUseListener(ModularSpells plugin, SpellProvider spellProvider) {
         this.plugin = plugin;
@@ -30,7 +37,9 @@ public class SpellBookUseListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (isSpellBookItem(event.getItem())) {
             Player player = event.getPlayer();
-            player.openInventory(getGui(player));
+            Inventory gui = getGui(player);
+            player.openInventory(gui);
+            openSpellBooks.add(gui);
         }
     }
 
@@ -56,5 +65,33 @@ public class SpellBookUseListener implements Listener {
             gui.addItem(spell.toItem());
         }
         return gui;
+    }
+
+    @EventHandler
+    public void onGuiClick(InventoryClickEvent event) {
+        if (isSpellBookGui(event.getClickedInventory())) {
+            ItemStack item = event.getCursor();
+            ItemMeta itemMeta = item.getItemMeta();
+            String name = itemMeta.getDisplayName();
+            Spell selectedSpell = spellProvider.getSpells()
+                    .stream()
+                    .filter(spell -> spell.getName().equals(name))
+                    .findFirst()
+                    .get();
+
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onGuiClose(InventoryCloseEvent event) {
+        Inventory inventory = event.getInventory();
+        if (isSpellBookGui(inventory)) {
+            openSpellBooks.remove(inventory);
+        }
+    }
+
+    private boolean isSpellBookGui(Inventory inventory) {
+        return openSpellBooks.contains(inventory);
     }
 }
